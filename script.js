5d0d94e413767edc5a8a08f9cbb39e0cdea68343
solovyovsdom <joelborofsky@mac.com>
2022-10-02 00:06:07 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector('.hamburger')
+const navLinks = document.querySelector('.nav-links')
+const links = document.querySelectorAll('.nav-links li')
+const nav = document.querySelector('.nav')
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open')
+    links.forEach(link => {
+        link.classList.toggle('fade')
+    })
+})
+
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
